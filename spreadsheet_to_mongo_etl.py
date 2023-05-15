38e44c8fbc807e5cad4e6ccf652f01559bc56942
Tshilidzi Emmanuel Mphelo <temphelo95@gmail.com>
2023-05-15 13:19:17 +0200

    establishing a connection to MongoDB, I'll be using my local DB

--- spreadsheet_to_mongo_etl.py
+++ spreadsheet_to_mongo_etl.py
@@ -4,3 +4,13 @@ from pymongo import MongoClient
 MONGO_URI = "mongodb://localhost:27017/"
 db_name = "spreadsheet_data"
 collection_name = "final_uso"
+
+data = pd.read_excel(file_path, engine='openpyxl')
+records = data.to_dict(orient='records')
+
+client = MongoClient(MONGO_URI)
+db = client[db_name]
+collection = db[collection_name]
+
+result = collection.insert_many(records)
+
