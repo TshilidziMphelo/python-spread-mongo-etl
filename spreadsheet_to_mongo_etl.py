da92e6e94051b7650b9e1d0d01b434b1f1e54cc2
Tshilidzi Emmanuel Mphelo <temphelo95@gmail.com>
2023-05-15 15:49:51 +0200

    Added some start and end times to calculate the execution times of the etl job

--- spreadsheet_to_mongo_etl.py
+++ spreadsheet_to_mongo_etl.py
@@ -1,12 +1,14 @@
 from pymongo import MongoClient
+import time
+from datetime import datetime
 
 # MongoDB connection details
 MONGO_URI = "mongodb://localhost:27017/"
-
 db_name = "spreadsheet_data"
 collection_name = "final_uso"
 
 def load_data_to_mongodb(file_path):
+    start_time = time.time()  # Start tracking execution time
     try:
         data = pd.read_excel(file_path, engine='openpyxl')
         records = data.to_dict(orient='records')  # Convert DataFrame to list of dictionaries
@@ -15,7 +17,9 @@ def load_data_to_mongodb(file_path):
         db = client[db_name]
         collection = db[collection_name]
 
-        result = collection.insert_many(records)
+        elapsed_time = time.time() - start_time
+
+	result = collection.insert_many(records)
     
     except Exception as e:
         raise
