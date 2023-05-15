57b704686dd0fad86e2567bbb2c7812e59f4fff2
Tshilidzi Emmanuel Mphelo <temphelo95@gmail.com>
2023-05-15 13:42:38 +0200

    moving the spreadsheet reading and mongo connection to a function and adding my main function

--- spreadsheet_to_mongo_etl.py
+++ spreadsheet_to_mongo_etl.py
@@ -5,12 +5,17 @@ MONGO_URI = "mongodb://localhost:27017/"
 db_name = "spreadsheet_data"
 collection_name = "final_uso"
 
-data = pd.read_excel(file_path, engine='openpyxl')
-records = data.to_dict(orient='records')
 
-client = MongoClient(MONGO_URI)
-db = client[db_name]
-collection = db[collection_name]
+def load_data_to_mongodb(file_path):
+    data = pd.read_excel(file_path, engine='openpyxl')
+    records = data.to_dict(orient='records')  # Convert DataFrame to list of dictionaries
+				     
+    client = MongoClient(MONGO_URI)
+    db = client[db_name]
+    collection = db[collection_name]
 
-result = collection.insert_many(records)
+    result = collection.insert_many(records)
 
+if __name__ == "__main__":
+    file_path = "FINAL_USO.xlsx"  # Path to your Excel file
+    load_data_to_mongodb(file_path)
