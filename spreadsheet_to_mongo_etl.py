4fce17939b9a7408208ed399b8fd59979770709d
Tshilidzi Emmanuel Mphelo <temphelo95@gmail.com>
2023-05-15 15:09:08 +0200

    Added some error handling and closing the mongo connection after loading the data to mongo

--- spreadsheet_to_mongo_etl.py
+++ spreadsheet_to_mongo_etl.py
@@ -2,19 +2,26 @@ from pymongo import MongoClient
 
 # MongoDB connection details
 MONGO_URI = "mongodb://localhost:27017/"
+
 db_name = "spreadsheet_data"
 collection_name = "final_uso"
 
-
 def load_data_to_mongodb(file_path):
-    data = pd.read_excel(file_path, engine='openpyxl')
-    records = data.to_dict(orient='records')  # Convert DataFrame to list of dictionaries
+    try:
+        data = pd.read_excel(file_path, engine='openpyxl')
+        records = data.to_dict(orient='records')  # Convert DataFrame to list of dictionaries
 				     
-    client = MongoClient(MONGO_URI)
-    db = client[db_name]
-    collection = db[collection_name]
+        client = MongoClient(MONGO_URI)
+        db = client[db_name]
+        collection = db[collection_name]
 
-    result = collection.insert_many(records)
+        result = collection.insert_many(records)
+    
+    except Exception as e:
+        raise
+    
+    finally:
+        client.close()
 
 if __name__ == "__main__":
     file_path = "FINAL_USO.xlsx"  # Path to your Excel file
